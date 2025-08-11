c861baf9bd0dfb4c15f65ea8608ab38c991477a8
RiZhii <143491670+RiZhii@users.noreply.github.com>
2025-08-11 14:31:29 +0530

    Update db-connection.php

--- php-sample-application-master/config-dev/db-connection.php
+++ php-sample-application-master/config-dev/db-connection.php
@@ -1,12 +1,9 @@
 <?php
-try {
-    $pdo = new PDO(
-        "mysql:host=127.0.0.1;port=3306;dbname=sampledb",
-        "admin",
-        "adminpass",
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-    echo "Database connection successful!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+return new PDO(
+    "mysql:host=127.0.0.1;port=3306;dbname=sampledb",
+    "admin",
+    "adminpass",
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+);
