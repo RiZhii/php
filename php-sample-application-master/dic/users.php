19ee64f97c666cd144c35e8510fc3032f4c5c028
RiZhii <143491670+RiZhii@users.noreply.github.com>
2025-08-11 14:33:07 +0530

    Update users.php

--- php-sample-application-master/dic/users.php
+++ php-sample-application-master/dic/users.php
@@ -1,6 +1,11 @@
 <?php
 require_once __DIR__ . '/../autoloader.php';
 
+$pdo = require __DIR__ . '/../config/db-connection.php';
+$usersService = new Service\UsersService($pdo);
+
+
+
 return new Service\UsersService(
     require "config/db-connection.php"
 );
