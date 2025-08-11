ed6b2e0f0fdbac032607493b1950e0a7b6f4060b
RiZhii <143491670+RiZhii@users.noreply.github.com>
2025-08-11 22:40:15 +0530

    Update Listing.php

--- php-sample-application-master/src/Views/Users/Listing.php
+++ php-sample-application-master/src/Views/Users/Listing.php
@@ -18,6 +18,7 @@ class Listing
             <div class="panel panel-info">
                 <div class="panel-body">
                     <h1>The newcomers:</h1>
+
                     <?php
                     /** @var \Entity\User $user */
                     foreach ($this->users as $user) {
@@ -28,11 +29,30 @@ class Listing
                                 <img alt="@<?= $userId ?> avatar" class="img-rounded" src="/img/<?= $userId ?>">
                             </a>
                             <div class="media-body">
-                                <p><a href="/<?= $userId ?>"><strong class="fullname"><?= $user->name ?></strong></a> <a href="/<?= $userId ?>">@<?= $userId ?></a></p>
-                                <small>joined <span class="time"><?= $user->joined ?></span></small>
+                                <p>
+                                    <a href="/<?= $userId ?>">
+                                        <strong class="fullname"><?= htmlspecialchars($user->name) ?></strong>
+                                    </a> 
+                                    <a href="/<?= $userId ?>">@<?= $userId ?></a>
+                                </p>
+                                <small>joined <span class="time"><?= htmlspecialchars($user->joined) ?></span></small>
                             </div>
                         </div>
                         <?php
+                    }
+                    ?>
+
+                    <hr>
+
+                    <h2>Alternate Listing (Simple Style)</h2>
+                    <?php
+                    foreach ($this->users as $user) {
+                        echo '<div class="user" style="display:flex;align-items:center;margin-bottom:10px;">';
+                        echo '<img src="' . htmlspecialchars($user->avatar) . '" alt="User avatar" style="border-radius:50%;width:50px;height:50px;margin-right:15px;">';
+                        echo '<div>';
+                        echo '<div class="name" style="font-weight:bold;">' . htmlspecialchars($user->name) . '</div>';
+                        echo '<div class="joined" style="font-size:0.9em;color:#777;">Joined: ' . htmlspecialchars($user->joined) . '</div>';
+                        echo '</div></div>';
                     }
                     ?>
                 </div>
